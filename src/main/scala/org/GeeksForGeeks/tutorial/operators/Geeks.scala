396900221ac604c3b4c5b1872b5da7d466bf265e
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-01-26 16:36:48 -0600

    feat(GeeksForGeeks.org tutorial)

--- src/main/scala/org/GeeksForGeeks/tutorial/operators/Geeks.scala
+++ src/main/scala/org/GeeksForGeeks/tutorial/operators/Geeks.scala
@@ -8,22 +8,22 @@ object Geeks extends App {
   val isFalse = false
   var result = 0
 
-  println ("10 + 4: "+ (x + four) )
-  println ("10 - 4: "+ (x - four) )
+  println ("10 + 4: "+ (x + four) ) // 14
+  println ("10 - 4: "+ (x - four) ) //  6
 
   if (x == four) {
     println ("Equal To Operator is True")
-  } else {
+  } else { // <<<
     println ("Equal To Operator is False")
   }
 
   // using Logical Operator 'OR'
-  println("(true || false): " + (isTrue || isFalse))
+  println("(true || false): " + (isTrue || isFalse)) // true
 
   // using Bitwise AND Operator
   result = x & four
-  println ("Bitwise AND: " + result )
+  println ("Bitwise AND: " + result ) // 0 // TODO understand WHY?
 
   // using Assignment Operators
-  println ("Addition Assignment Operator: " + (x += four) )
+  println ("Addition Assignment Operator: " + (x += four) ) // () // TODO understand WHY?
 }
