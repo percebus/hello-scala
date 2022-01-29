3feaa23cbb7f98282c174abaaab8dee1be9b7d4e
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-01-29 10:13:08 -0600

    feat(setsApp)

--- src/main/scala/org/GeeksForGeeks/tutorial/collections/lists/listsApp.scala
+++ src/main/scala/org/GeeksForGeeks/tutorial/collections/lists/listsApp.scala
@@ -10,10 +10,10 @@ import scala.collection.immutable._ // XXX? works w/o it
   * explicit: List(Geeks, GFG, GeeksforGeeks, Geek123)
   * implicit: List(C, C#, Java, Scala, PHP, Ruby)
   *
-  * for each
+  * iterator
   * C C# Java Scala PHP Ruby
   *
-  * iterator
+  * for each
   * C C# Java Scala PHP Ruby
   * Process finished with exit code 0
   */
@@ -25,14 +25,16 @@ object listsApp extends App {
   println("explicit: "+explicitList)
   println("implicit: "+implicitList)
 
-  println("\nfor each")
-  for (item <- implicitList) {
-    print(item+" ")
-  }
-
-  println("\n\niterator")
+  println("\niterator")
   val oIterator:Iterator[String] = implicitList.iterator
   while (oIterator.hasNext) {
     print(oIterator.next+" ")
   }
+
+
+  println("\n\nfor each")
+  for (item <- implicitList) {
+    print(item+" ")
+  }
+
 }
---
+++ src/main/scala/org/GeeksForGeeks/tutorial/collections/sets/setsApp.scala
@@ -0,0 +1,57 @@
+package org.GeeksForGeeks.tutorial.collections.sets
+
+import scala.collection.Iterator
+import scala.collection.immutable._ // XXX? works w/o it
+
+
+/** Output:
+  *
+  * Sets
+  * explicit: Set(Geeks, GFG, GeeksforGeeks, Geek123)
+  * implicit: Set(Scala, C#, Ruby, PHP, C, Java)
+  *
+  * iterator
+  * Scala C# Ruby PHP C Java
+  *
+  * for each
+  * Scala C# Ruby PHP C Java
+  *
+  * .foreach( x => do(x) )
+  * Scala C# Ruby PHP C Java
+  * Process finished with exit code 0
+  */
+object listsApp extends App {
+  val explicitSet:Set[String] = Set(
+    "Geeks", "Geeks",
+    "GFG", "GFG", "GFG",
+    "GeeksforGeeks", "GeeksforGeeks",
+    "Geek123"
+  )
+
+  val implicitSet = Set(
+    "C", "C", "C", "C",
+    "C#", "C#", "C#",
+    "Java", "Java",
+    "Scala",
+    "PHP", "PHP",
+    "Ruby")
+
+  println("Sets")
+  println("explicit: "+explicitSet) // duplicates are removed
+  println("implicit: "+implicitSet) // duplicates are removed
+
+  println("\niterator")
+  val oIterator:Iterator[String] = implicitSet.iterator
+  while (oIterator.hasNext) {
+    print(oIterator.next+" ")
+  }
+
+  println("\n\nfor each")
+  for (item <- implicitSet) {
+    print(item+" ")
+  }
+
+  println("\n\n.foreach( x => do(x) )")
+  implicitSet
+    .foreach((item:String) => print(item+" "))
+}
